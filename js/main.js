04508f9b570b73cd61926bd3791c8a9c352b6f84
Valera Savchenko <ivalera.develop@gmail.com>
2024-01-14 17:37:45 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const sendForm = document.querySelector('.chat__form-send');
+const msgInput = document.querySelector('.chat__send-text');
+const msgTemplate = document.querySelector('#message');
+const msgList = document.querySelector('.chat__messages-list');
+
+sendForm.addEventListener('submit', sendMessage);
+
+function sendMessage(event) {
+    event.preventDefault()
+
+    const msgText = msgInput.value;
+    if(!msgText){
+        return;
+    }
+    const templateContent = msgTemplate.content.cloneNode(true)
+    templateContent.querySelector('.chat__message-text').textContent = `Я: ${msgText}`;
+    msgList.append(templateContent);
+    msgInput.value = '';
